96581c4621e6b7bec493a5b99d6dc78460c3520c
AnubhavBhatnagar27 <anubhav276bhatnagar@gmail.com>
2024-09-09 21:08:09 +0530

    Third Commi

---
+++ lecture4.cpp
@@ -0,0 +1,108 @@
+// Pattern 1
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter a number: ";
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+// return 0;
+// }
+
+
+
+// Pattern 2
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter a number: ";
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// return 0;
+// }
+
+// Pattern 3
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter a number: ";
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//     char ch='A';
+//         for(int j=1;j<=n;j++){
+//             cout<<ch;
+//             ch=ch+1;
+//         }
+//         cout<<endl;
+//     }
+// return 0;
+// }
+
+
+// Pattern 4
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     int nums=1;
+//     cout<<"Enter a number: ";
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<nums<<"\t";
+//             nums+=1;
+//         }
+//         cout<<endl;
+//     }
+// return 0;
+// }
+
+// Pattern 5
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     char ch='A';
+//     cout<<"Enter a number: ";
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<ch<<"\t";
+//             ch+=1;
+//         }
+//         cout<<endl;
+//     }
+// return 0;
+// }
+
+
+// Pattern 6 Triangle Pattern
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter a number: ";
+//     cin>>n;
+//     for(int i=0;i<=n;i++){
+//         for(int j=1;j<=i+1-1;j++){
+//             cout<<"*";
+//         }
+//     cout<<endl;
+//     }
+// return 0;
+// }
+
+
+// Pattern 7
