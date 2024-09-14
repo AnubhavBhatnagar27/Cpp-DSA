2190ae819e8860e9dc2cf5a6470f657687a3c003
AnubhavBhatnagar27 <anubhav276bhatnagar@gmail.com>
2024-09-14 16:34:59 +0530

    Lecture 5 commit

--- lecture5.cpp
+++ lecture5.cpp
@@ -17,32 +17,63 @@
 
 // Calculate nCr binomial coefficient n and r
 // nCr=n!/r!(n-r)!
-#include<bits/stdc++.h>
-using namespace std;
-int factorial(int n){
-    int fact =1;
-    for (int i = 1; i <= n; i++)
-    {
-        fact *=i;
-    }
-    return fact;
+// #include<bits/stdc++.h>
+// using namespace std;
+// int factorial(int n){
+//     int fact =1;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         fact *=i;
+//     }
+//     return fact;
     
-}
-int nCr(int n,int r){
-    //n factorial
-    int fact_n=factorial(n);
-    //r factorial
-    int fact_r=factorial(r);
-    //n-r factorial
-    int fact_ncr=factorial(n-r);
-    return fact_n/(fact_r*fact_ncr);
-}
-int main(){
-    int n,r;
-    cout<<"Enter the value of n: ";
-    cin>>n;
-    cout<<"Enter the value of r: ";
-    cin>>r;
-    cout<<nCr(n,r);
-return 0;
-}
+// }
+// int nCr(int n,int r){
+//     //n factorial
+//     int fact_n=factorial(n);
+//     //r factorial
+//     int fact_r=factorial(r);
+//     //n-r factorial
+//     int fact_ncr=factorial(n-r);
+//     return fact_n/(fact_r*fact_ncr);
+// }
+// int main(){
+//     int n,r;
+//     cout<<"Enter the value of n: ";
+//     cin>>n;
+//     cout<<"Enter the value of r: ";
+//     cin>>r;
+//     cout<<nCr(n,r);
+// return 0;
+// }
+
+
+// WAF to check a number is prime of not
+// #include<bits/stdc++.h>
+// using namespace std;
+// bool prime(int n){
+//     if(n<=1) return false;
+//     if(n<=3 && n>1) return true;
+//     if (n % 2 == 0 || n % 3 == 0) return false;
+//     for(int i=5;i*1<=n-1;i++){
+//         if(n%i==0){
+//             return false;
+//         }
+//         else{
+//             return true;
+//         }
+//     }
+//     return 0;
+// }
+// int main(){
+//     int n;
+//     cout<<"Enter a number: ";
+//     cin>>n;
+//     if(prime(n)){
+//         cout<<"A prime number.";
+//     }
+//     else{
+//         cout<<"Not a Prime number.";
+//     }
+// return 0;
+// }
