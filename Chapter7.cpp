fbe524ed4b1ee0053d5d9e2de69d1bc80b655abe
AnubhavBhatnagar27 <anubhav276bhatnagar@gmail.com>
2024-09-18 05:06:42 +0530

    Chapter 7

---
+++ Chapter7.cpp
@@ -0,0 +1,24 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a=4,b=8;
+//     cout<<(a&b)<<endl;
+// return 0;
+// }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a=4,b=8;
+//     cout<<(a|b)<<endl;
+// return 0;
+// }
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=4,b=8;
+    cout<<(a|b)<<endl;
+return 0;
+}
