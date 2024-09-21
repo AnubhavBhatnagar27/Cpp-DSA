9f012fbc3f9fcd91a1ec317a6f2b232d806b74d9
AnubhavBhatnagar27 <anubhav276bhatnagar@gmail.com>
2024-09-21 20:23:30 +0530

    STL in C++

--- MANIT_Day1.cpp
+++ MANIT_Day1.cpp
@@ -50,3 +50,40 @@
 //         return ans;
 //         }
 // };
+
+// Question 238
+// Brute Force Solution
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         vector<int> answer;
+//         for(int i=0;i<nums.size();i++){
+//             int p=1;
+//             for(int j=0;j<nums.size();j++){
+//                 if(i==j) continue;
+//                 p*=nums[j];
+//             }
+//             answer.push_back(p);
+//         }
+//         return answer;
+//     }
+// };
+
+// Best Approach
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> output(n);
+        output[0] = 1;
+        for(int i=1; i<n; i++){
+            output[i] = output[i-1] * nums[i-1];
+        }
+        int right = 1;
+        for(int i=n-1; i>=0; i--){
+            output[i] *= right;
+            right *= nums[i];
+        }
+        return output;
+    }
+};
