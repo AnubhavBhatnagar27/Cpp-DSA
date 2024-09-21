b6df00ea0ef40e1bdac6f009268bb4cd52643dd9
Anubhav Bhatnagar <134833705+AnubhavBhatnagar27@users.noreply.github.com>
2024-09-21 18:28:55 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,33 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         unordered_map<int,int> mp;
+//         for(int i=0;i<nums.size();i++){
+//             int find=target-nums[i];
+//             if(mp.count(find)){
+//                 return {i,mp[find]};
+//             }
+//             mp[nums[i]]=i;
+//         }
+//         return {};
+//     }
+// };
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int>d;
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            int ele=nums[i];
+            while(!d.empty() && d.back()<nums[i]){
+                d.pop_back();
+            }
+            d.push_back(i);
+            if(i>=k && nums[i-k]==d.front()) d.pop_front();
+            if(i>=k-1) ans.push_back(nums[d.front()]);
+
+        }
+        return ans;
+    }
+};
