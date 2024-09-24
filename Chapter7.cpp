96b67bf9133c1532391418cfde2466f8c07393a2
AnubhavBhatnagar27 <anubhav276bhatnagar@gmail.com>
2024-09-24 21:29:23 +0530

    Lecture 7 Updated

--- Chapter7.cpp
+++ Chapter7.cpp
@@ -18,13 +18,32 @@
 // #include<bits/stdc++.h>
 // using namespace std;
 // int main(){
-//     int a=4,b=8;
+//     int a=3,b=7;
+//     cout<<(a^b)<<endl;
+// return 0;
+// }
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a=3,b=7;
 //     cout<<(a|b)<<endl;
 // return 0;
 // }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a=3,b=7;
+//     cout<<(a&b)<<endl;
+// return 0;
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout<<"Hello World";
+    int a=3,b=7;
+    cout<<(a<<2)<<endl;
 return 0;
 }
