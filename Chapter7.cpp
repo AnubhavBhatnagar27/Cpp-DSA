5e02c1e3f0dfd620971f4f8e2fbdc6e9b94ca319
AnubhavBhatnagar27 <anubhav276bhatnagar@gmail.com>
2024-09-24 22:07:43 +0530

    Lecture 7 Updated

--- Chapter7.cpp
+++ Chapter7.cpp
@@ -40,10 +40,68 @@
 // return 0;
 // }
 
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a=3,b=7;
+//     cout<<(a<<2)<<endl;
+// return 0;
+// }
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a=3,b=7;
+//     cout<<(b>>2)<<endl;
+// return 0;
+// }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a=6,b=10;
+//     cout<<(a&b)<<endl;
+//     cout<<(a|b)<<endl;
+//     cout<<(a^b);
+// return 0;
+// }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a=10,b=2,c=1;
+//     cout<<(a<<b)<<endl;
+//     cout<<(a>>c);
+// return 0;
+// }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     unsigned int x=-10;
+//     cout<<sizeof(int)<<endl;
+//     cout<<sizeof(long int)<<endl;
+//     cout<<sizeof(short int)<<endl;
+//     cout<<sizeof(long long)<<endl;
+//     cout<<x<<endl;
+// return 0;
+// }
+
+// Figure out how to find if a number is power of 2 without any loop.
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int a=3,b=7;
-    cout<<(a<<2)<<endl;
+    int n;
+    cout<<"Enter a number:";
+    cin>>n;
+    int pow=1;
+    while(pow<n){
+        pow<<=1;
+    }
+    return pow==n;
+    if(pow==n){
+        cout<<"The number is of power of 2";
+    }
 return 0;
 }
+
+// WAF ato reverse an integer n
